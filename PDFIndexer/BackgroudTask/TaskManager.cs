fe3116426ff6cef9a9e91c672bf448c00a7e04a4
Hyeong-Jong Choi <0x@686a.io>
2025-11-24 19:02:08 +0900

    Add background task manager

---
+++ PDFIndexer/BackgroudTask/TaskManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PDFIndexer.BackgroundTask
+{
+    internal class TaskManager
+    {
+        private static Queue<AbstractTask> Tasks;
+
+        private Thread TaskThread;
+        private bool NeedToStop = false;
+
+#if DEBUG
+        private static readonly int EmptyTaskPenalty = 3000;
+        private static readonly int DelayPerTask = 1000;
+#else
+        private static readonly int EmptyTaskPenalty = 30 * 1000;
+        private static readonly int DelayPerTask = 3000;
+#endif
+
+        public TaskManager()
+        {
+            Tasks = new Queue<AbstractTask>();
+            TaskThread = new Thread(TaskRunner);
+        }
+
+        public void Start()
+        {
+            NeedToStop = false;
+            TaskThread.Start();
+        }
+
+        public void Stop()
+        {
+            NeedToStop = true;
+            TaskThread.Join();
+        }
+
+        private void TaskRunner()
+        {
+            while (!NeedToStop)
+            {
+                // Empty task queue panelty
+                if (Tasks.Count == 0)
+                {
+                    Thread.Sleep(EmptyTaskPenalty);
+                    continue;
+                }
+
+                var task = Tasks.Dequeue();
+                task.Run();
+
+                Thread.Sleep(DelayPerTask);
+            }
+        }
+
+        public static void Enqueue(AbstractTask task)
+        {
+            Tasks.Enqueue(task);
+        }
+    }
+}
