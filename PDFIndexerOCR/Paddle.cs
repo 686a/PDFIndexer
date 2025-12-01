66064afe98f6a0c1d60cdca0de1a06c35699247f
Hyeong-Jong Choi <0x@686a.io>
2025-12-01 20:03:26 +0900

    Optimization OCR

--- PDFIndexerOCR/Paddle.cs
+++ PDFIndexerOCR/Paddle.cs
@@ -10,15 +10,17 @@ namespace PDFIndexerOCR
 {
     internal class Paddle
     {
-//#if DEBUG
-//        private const bool debug = true;
-//#else
-//        private const bool debug = false;
-//#endif
+        //#if DEBUG
+        //        private const bool debug = true;
+        //#else
+        //        private const bool debug = false;
+        //#endif
 
         FullOcrModel model = LocalFullModels.KoreanV4;
-
+        
         Action<PaddleConfig> device;
+
+        private PaddleOcrAll OCRInstance;
 
         public Paddle()
         {
@@ -38,24 +40,22 @@ namespace PDFIndexerOCR
                     useCpuThreads = 0;
                     break;
             }
+
             device = PaddleDevice.Onnx(cpuMathThreadCount: useCpuThreads, glogEnabled: false);
+
+            OCRInstance = new PaddleOcrAll(model, device)
+            {
+                AllowRotateDetection = true,
+            };
         }
 
         public PaddleOcrResult OCR(byte[] image)
         {
-            using (PaddleOcrAll all = new PaddleOcrAll(model, device)
+            using (Mat src = Cv2.ImDecode(image, ImreadModes.Color))
             {
-                // 요거 켜면 정확도 낮아짐
-                //AllowRotateDetection = true,
-                //Enable180Classification = true,
-            })
-            {
-                using (Mat src = Cv2.ImDecode(image, ImreadModes.Color))
-                {
-                    PaddleOcrResult result = all.Run(src);
+                PaddleOcrResult result = OCRInstance.Run(src);
 
-                    return result;
-                }
+                return result;
             }
         }
     }
